a9f9a000907c135f021cc4325a102e3b80f98332
ALIEN-ARTHUR\arthur <arthurcoelho.27@gmail.com>
2023-03-10 09:32:47 -0300

    first commit

---
+++ src/main/java/br/com/hateos/RestSpringBootHateosApplication.java
@@ -0,0 +1,13 @@
+package br.com.hateos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootHateosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBootHateosApplication.class, args);
+	}
+
+}
